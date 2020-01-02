8bd010c69c54674f36386ec306e798e1bacecc73
Anthony Ronca <Anthonythomronca@gmail.com>
2020-01-02 01:15:44 -0800

    MagicHat Initial Commit

---
+++ MagicHat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MagicHat
+//
+//  Created by Anthony Ronca on 1/1/20.
+//  Copyright © 2020 AnthonyRonca. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
