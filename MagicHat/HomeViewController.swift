61a4d6b2e80e76762b4a8ca03870a61949930724
Anthony Ronca <Anthonythomronca@gmail.com>
2020-01-14 14:20:29 -0800

    Added design to launch screen, initial home screen design, and attached ViewController to hold event handlers

---
+++ MagicHat/HomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.swift
+//  MagicHat
+//
+//  Created by Anthony Ronca on 1/14/20.
+//  Copyright © 2020 AnthonyRonca. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    
+    /* BUTTONS */
+    
+    @IBOutlet var image_OnClick: UIImageView!
+    
+    @IBOutlet var addTicket_OnClick: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
